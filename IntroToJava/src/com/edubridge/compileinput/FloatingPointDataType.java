7204d322b39040a1857b1da705e7176c2b533bf3
lenovo <lenovo@lenovo-PC-7>
2024-01-19 11:12:01 +0530

    second commit

---
+++ IntroToJava/src/com/edubridge/compileinput/FloatingPointDataType.java
@@ -0,0 +1,14 @@
+package com.edubridge.compileinput;
+import java.util.Scanner;
+
+
+public class FloatingPointDataType {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		float x=s.nextFloat();
+		double y=s.nextDouble();
+		System.out.println(x);
+		System.out.println(y);;
+	}
+
+}
