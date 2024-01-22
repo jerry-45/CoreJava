f64f5edf6888229bb46d7c019fdeb288ea8b3dfa
jerry-45 <vishwa.atul16@gmail.com>
2024-01-22 19:54:37 +0530

    Boolean Data Type

---
+++ IntroToJava/src/com/edubridge/compileinput/BooleanDataType.java
@@ -0,0 +1,18 @@
+package com.edubridge.compileinput;
+
+import java.util.Scanner;
+
+public class BooleanDataType {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		char c = s.next().charAt(2);
+		String str = s.next();
+		boolean b = false;
+		System.out.println(b);
+		System.out.println("The Character Value is "+c);
+		System.out.println("The String Value is "+str);
+
+	}
+
+}
