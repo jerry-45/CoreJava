daa492e4a044634c2c04c0a843a9f9b54863b1fb
jerry-45 <vishwa.atul16@gmail.com>
2024-01-22 12:40:32 +0530

    Do While Loop

---
+++ ControlStructureStatements/src/com/edubridge/loopingstatements/DoWhileLoop.java
@@ -0,0 +1,13 @@
+package com.edubridge.loopingstatements;
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) {
+		int a = 22;
+		do {
+			System.out.print(a + " ");
+			a++;
+		}
+		while(a <= 22);
+	}
+}
