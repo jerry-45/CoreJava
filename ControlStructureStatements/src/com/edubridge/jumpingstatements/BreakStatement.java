3f4b6a06e078cd9538002f9c4f46b3fb5c67eba6
jerry-45 <vishwa.atul16@gmail.com>
2024-01-22 13:27:01 +0530

    Jumping statements

---
+++ ControlStructureStatements/src/com/edubridge/jumpingstatements/BreakStatement.java
@@ -0,0 +1,14 @@
+package com.edubridge.jumpingstatements;
+
+public class BreakStatement {
+
+	public static void main(String[] args) {
+		for(int i = 1; i <=10; i++) {
+			if(i==5)
+				break;
+			
+			System.out.println(i);
+		}
+	}
+
+}
