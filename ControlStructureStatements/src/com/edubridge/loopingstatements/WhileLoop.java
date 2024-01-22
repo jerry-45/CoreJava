d158b4f37beefd22840e4d1f2677c0e6e0207230
jerry-45 <vishwa.atul16@gmail.com>
2024-01-22 12:04:01 +0530

    while loop

---
+++ ControlStructureStatements/src/com/edubridge/loopingstatements/WhileLoop.java
@@ -0,0 +1,29 @@
+package com.edubridge.loopingstatements;
+
+import java.util.Scanner;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		int i = 0;
+		
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println("Enter a value for n:");
+		int n = s.nextInt();
+		
+		System.out.println("Using Incrementer:");
+		while(i <= n) {
+			System.out.print(i + " ");
+			i++;
+		}
+		
+		System.out.println("\nUsing Decrementer:");
+		while(n >= 0) {
+			System.out.print(n + " ");
+			n--;
+		}
+
+	}
+
+}
