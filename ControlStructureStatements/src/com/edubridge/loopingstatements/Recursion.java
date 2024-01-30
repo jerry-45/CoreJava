b602bcd70ec9429661476b4cab6d34d22ba77b3a
lenovo <lenovo@lenovo-PC>
2024-01-30 12:48:27 +0530

    Recursion

---
+++ ControlStructureStatements/src/com/edubridge/loopingstatements/Recursion.java
@@ -0,0 +1,27 @@
+package com.edubridge.loopingstatements;
+
+import java.util.Scanner;
+
+public class Recursion {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter a number : ");
+		int number = s.nextInt();
+		int fact=1;
+		
+		for(int i = 1; i <= number; i++) {
+			if(number == 0)
+				System.out.println("The factorial " + number + " is " + 1);
+			else
+				fact = fact*i;
+				
+		}
+		
+		System.out.println("The factorial " + number + " is " + fact);
+		
+		s.close();
+
+	}
+
+}
