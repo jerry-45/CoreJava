bb9468300113e413f157d8e9a8f95c6cff5cb331
lenovo <lenovo@lenovo-PC>
2024-01-30 12:10:26 +0530

    Enumeration Example "Day.java"

---
+++ EnumerationFunctionAndRecursion/src/com/edubridge/enumeration/Day.java
@@ -0,0 +1,20 @@
+package com.edubridge.enumeration;
+
+public enum Day {
+	SUNDAY(0), MONDAY(1), TUESDAY(2), WEDNESDAY(3), THRUSDAY(4), FRIDAY(5), SATURDAY(6);
+	
+	private int i;
+
+	Day(int index) {
+		i = index;
+	}
+	
+	int displayIndex() {
+		return i;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(Day.SATURDAY);
+		System.out.println(Day.MONDAY.displayIndex());
+	}
+}
