13fc02e34515f7d15b077f9d12d742a3929f7665
lenovo <lenovo@lenovo-PC>
2024-01-30 12:40:41 +0530

    Factorial using recursion

--- EnumerationFunctionAndRecursion/src/com/edubridge/enumeration/Day.java
+++ EnumerationFunctionAndRecursion/src/com/edubridge/enumeration/Day.java
@@ -1,5 +1,7 @@
 package com.edubridge.enumeration;
 
+// program to demonstrate enumeration
+
 public enum Day {
 	SUNDAY(0), MONDAY(1), TUESDAY(2), WEDNESDAY(3), THRUSDAY(4), FRIDAY(5), SATURDAY(6);
 	
---
+++ EnumerationFunctionAndRecursion/src/com/edubridge/recursion/Recursion.java
@@ -0,0 +1,23 @@
+package com.edubridge.recursion;
+
+import java.util.Scanner;
+
+public class Recursion {
+	
+	static int factorial(int n) {
+		if(n == 0)
+			return 1;
+		else
+			return n*factorial(n-1);
+	}
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int number = s.nextInt();
+		System.out.println();
+		
+		System.out.println("Factorial of " + number + " is " + factorial(number));
+	}
+
+}
