750fde233bb1b7077b2ca650130902ecabb2eddb
jerry-45 <vishwa.atul16@gmail.com>
2024-02-05 19:16:56 +0530

    Assignment: Enum Example

---
+++ Assignments/src/com/edubridge/miscellaneous/EnumExample.java
@@ -0,0 +1,39 @@
+package com.edubridge.miscellaneous;
+
+/*
+	Write a Java program to implement an enum called "Direction" with constants 
+	representing the cardinal directions (North, South, East, West).
+*/
+
+//Enum definition for Direction
+enum Direction {
+	NORTH,
+	SOUTH,
+	EAST,
+	WEST
+}
+
+public class EnumExample {
+	public static void main(String[] args) {
+        // Using the enum constants
+        Direction myDirection = Direction.NORTH;
+
+        // Switch statement to demonstrate usage
+        switch (myDirection) {
+            case NORTH:
+                System.out.println("Heading North");
+                break;
+            case SOUTH:
+                System.out.println("Heading South");
+                break;
+            case EAST:
+                System.out.println("Heading East");
+                break;
+            case WEST:
+                System.out.println("Heading West");
+                break;
+            default:
+                System.out.println("Invalid direction");
+        }
+    }
+}
