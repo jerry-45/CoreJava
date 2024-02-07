dbf19f385ed9bb74dfa9b8fb432074f80c400ddf
jerry-45 <vishwa.atul16@gmail.com>
2024-02-07 19:09:55 +0530

    Assignment

---
+++ Assignments/src/com/edubridge/miscellaneous/InheritanceAssignment.java
@@ -0,0 +1,17 @@
+package com.edubridge.miscellaneous;
+
+// Date: 6th February 2024
+// Write a Java program to create a class known as "BankAccount" with 
+// methods called deposit() and withdraw(). Create a subclass called 
+// SavingsAccount that overrides the withdraw() method to prevent withdrawals 
+// if the account balance falls below one hundred
+
+
+public class InheritanceAssignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
