73705f8387490c3c7ae9dd124b04e05eb07ce324
jerry-45 <vishwa.atul16@gmail.com>
2024-02-13 12:52:02 +0530

    equals() and hashCode()

---
+++ JavaCollectionFramework/src/com/edubridge/equalsandhashcodemethod/Site.java
@@ -0,0 +1,55 @@
+package com.edubridge.equalsandhashcodemethod;
+
+import java.util.Objects;
+
+// Program to demonstrate on equals() method
+public class Site {
+	private String name;
+	private Integer id;
+	
+	// Only using hashCode() method, when we compare two objects, 
+	// we will not get the comparison as true... for that we need 
+	// to override equals method
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+	
+	// Only using equals() method, when we compare two objects, 
+	// we will not get the comparison as true... for that we need 
+	// to override equals method
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Site other = (Site) obj;
+		return Objects.equals(id, other.id) && Objects.equals(name, other.name);
+	}
+
+	public Site(String name, Integer id) {
+		super();
+		this.name = name;
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+}
--- JavaCollectionFramework/src/com/edubridge/equalsandhashcodemethod/SiteExecutor.java
+++ JavaCollectionFramework/src/com/edubridge/equalsandhashcodemethod/SiteExecutor.java
@@ -1,4 +1,4 @@
-package com.edubridge.equalsmethod;
+package com.edubridge.equalsandhashcodemethod;
 
 public class SiteExecutor {
 
