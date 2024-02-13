14fb0c49fe60801fffbbfa51dd7db1e646259b71
jerry-45 <vishwa.atul16@gmail.com>
2024-02-13 12:38:17 +0530

    equals() and hashCode()

--- JavaCollectionFramework/src/com/edubridge/equalsmethod/SiteExecutor.java
+++ JavaCollectionFramework/src/com/edubridge/equalsmethod/SiteExecutor.java
@@ -6,13 +6,19 @@ public class SiteExecutor {
 		String str1 = "Creativity";
 		String str2 = "Creativity";
 		System.out.println(str1.equals(str2));
+		System.out.println(str1.hashCode());
+		System.out.println(str2.hashCode());
 		
 		String str3 = new String("Creativity");
 		String str4 = new String("Creativity");
 		System.out.println(str3.equals(str4));
+		System.out.println(str3.hashCode());
+		System.out.println(str4.hashCode());
 		
 		Site s1 = new Site("Creativity", 1);
 		Site s2 = new Site("Creativity", 1);
 		System.out.println(s1.equals(s2));
+		System.out.println(s1.hashCode());
+		System.out.println(s2.hashCode());
 	}
 }
