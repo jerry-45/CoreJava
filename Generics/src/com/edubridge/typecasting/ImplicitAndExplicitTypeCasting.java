0568ce8c488a7730be283100d71f272951418c1f
jerry-45 <vishwa.atul16@gmail.com>
2024-02-13 13:35:54 +0530

    Implicit Typecasting (Widening) and Explicit Typecasting (Narrowing)

--- Generics/src/com/edubridge/typecasting/ImplicitAndExplicitTypeCasting.java
+++ Generics/src/com/edubridge/typecasting/ImplicitAndExplicitTypeCasting.java
@@ -4,11 +4,13 @@ public class ImplicitAndExplicitTypeCasting {
 
 	public static void main(String[] args) {
 		// Implicit Typecasting or Widening
+		// smaller to larger data type size
 		char c = 'b';
 		int num = c;
 		System.out.println(num);
 		
 		// Explicit Typecasting or Narrowing
+		// larger to smaller data type size
 		int num1 = 63;
 		char x = (char) num1;
 		System.out.println(x);
