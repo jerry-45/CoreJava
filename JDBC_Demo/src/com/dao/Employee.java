ebc39c5adba732a7c69ba283af37f5ec9b3474cf
jerry-45 <vishwa.atul16@gmail.com>
2024-02-27 13:13:42 +0530

    JDBC CRUD Operation

--- JDBC_Demo/src/com/dao/Employee.java
+++ JDBC_Demo/src/com/dao/Employee.java
@@ -20,11 +20,25 @@ public class Employee {
 		// Step 3 - Call DriverManager class
 		Connection conn = DriverManager.getConnection(url, username, password); 	// Establish connection with the database
 				
-		System.out.println("Collect data from employee table.");
-				
 		// The object used for executing a static SQL statements
 		Statement stmt = conn.createStatement();		// statement help me to execute the SQL query
-				
+		
+		System.out.println("Inserting data into table...");
+		String insertQuery = "INSERT INTO employee VALUES (108, 'Atul', 22);";
+		stmt.execute(insertQuery);
+		System.out.println("Data inserted successfully");
+		
+		System.out.println("Updating data...");
+		String updateQuery = "UPDATE employee SET name='Shreyash' WHERE id=104;";
+		stmt.execute(updateQuery);
+		System.out.println("Update successfull");
+		
+		System.out.println("Deleting data...");
+		String deleteQuery = "DELETE FROM employee WHERE id = 105;";
+		stmt.execute(deleteQuery);
+		System.out.println("Seletion Successfull");
+		
+		System.out.println("Collect data from employee table.");
 		String query = "SELECT * FROM employee";
 				
 		// A table of data representing a database result set, which is 
